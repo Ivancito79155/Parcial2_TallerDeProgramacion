06c91becdbe45b302c302e75fca47d2e2fe8c527
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-06-27 15:48:18 -0300

    Arreglos menosres en sintaxis

--- app.js
+++ app.js
@@ -1,54 +1,54 @@
-const express = require("express")
+const express = require("express");
 const books = require("./db");
-const { get } = require("http");
 
 const app = express();
 
 app.use(express.text);
 app.use(express.json);
 
-app.get("/",(req,res)=>{
-    res.send(`<h1> HOME </h1>`)
-})
+app.get("/", (req, res) => {
+  res.send(`<h1> HOME </h1>`);
+});
 
-app.get("/books",(req,res)=>{
-    res.json(books)
-})
+app.get("/books", (req, res) => {
+  res.json(books);
+});
 
-app.get("/books/:id",(req,res) => {
-    const id = parseInt(req.params.id)
-    const getUser = books.find((title) => books.id === id)
-    if (getUser) {
-        console.log("Se encontró el libro");
-    }
-    else{
-        console.log("No se encontró el libro buscado");
-    }
-})
+app.get("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const getTitle = books.find((title) => title.id === id);
+  if (getTitle) {
+    console.log("Se encontró el libro");
+  } else {
+    console.log("No se encontró el libro buscado");
+  }
+});
 
-app.post("/books",(req,res)=>{
-    const id = new Date().getTime();
-    const {title, author, year} = req.body;
-    const newBook = books.push
-    res.json({message:`Se subio el libro ${title}`})
-})
+app.post("/books", (req, res) => {
+  const id = new Date().getTime();
+  const { title, author, year } = req.body;
+  const newBook = books.push({id:id,title:title,author:author,year:year});
+  res.json({ message: "Se subio el nuevo libro" });
+});
 
-app.put("/books/:id",(req,res) => {
-    const id = parseInt(req.params.id);
-    const {title,author,year} = req.body;
-    const getBook = books.find((title) => books.id === id)
-    getBook.title = title;
-    getBook.author = author;
-    getBook.year = year;
-})
+app.put("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const { title, author, year } = req.body;
+  const getBook = books.find((title) => title.id === id);
+  getBook.title = title;
+  getBook.author = author;
+  getBook.year = year;
+  res.json({ message: "La actualización resulto exitosa" });
+});
 
-app.delete("/books/:id",(req,res)=>{
-    const id = parseInt(req.params.id);
-    const getBook = books.find(title);
-    const getIndex = books.indexOf(getBook);
-    const deleteBook = books.splice(getIndex,1)
-})
+app.delete("/books/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const getBook = books.find(title);
+  const getIndex = books.indexOf(getBook);
+  const deleteBook = books.splice(getIndex, 1);
+  res.json({ message: "Se eliminó con éxito el libro" });
+});
 
-app.listen(2700,()=>{
-    console.log("Servidor activo");
-})
+app.listen(2700, () => {
+  console.log("Servidor activo");
+});
--- db.js
+++ db.js
@@ -1,16 +1,5 @@
 const books = [
-    {id:1,
-    title:"El principito",
-    author:"Antoine de Saint-Exupéry",
-    year:"1993"},
-    {id:1,
-    title:"Don Quijote de la Mancha",
-    author:"Miguel de Cervantes",
-    year:"1605"},
-    {id:3,
-    title:"El ladrón del rayo",
-    author:"Rick Riordan",
-    year:"2005"}
-]
+
+];
 
 module.exports = books
